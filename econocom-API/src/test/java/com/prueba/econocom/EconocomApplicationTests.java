30e26045defb82e1e89412f66c791d98bbf0b21e
Rolan Muñoz <rolanmunoz.dev@gmail.com>
2025-07-07 12:15:20 +0200

    initial commit

---
+++ econocom-API/src/test/java/com/prueba/econocom/EconocomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.econocom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconocomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
